a88c8bbefed462b6fe19d730144648e9fdb797e4
Kenneth Pinzon <kennethpinzonrojas@gmail.com>
2022-09-10 10:15:28 -0500

    videoplayer function and app.css update to sanserif/nomediacontrols

---
+++ src/VideoPlayer.js
@@ -0,0 +1,38 @@
+import React, {useEffect, useRef} from "react";
+import "./App.css";
+
+export default function App(){
+    const videoEl =useRef(null);
+
+    const attemptPlay = () => {
+        videoEl &&
+        videoEl.current &&
+        videoEl.current.play().catch(error => {
+            console.error("Error attempting to play", error);
+        });
+    };
+
+    useEffect(() => {
+        attemptPlay();
+    }, []);
+
+    return(
+        <div> className = "App"
+            <h1>Autoplay example</h1>
+                <div>
+                    <video
+                    style={{ maxWidth: "100%", width: "800px", margin: "0 auto" }}
+                     playsInline
+                     loop
+                    muted
+                    controls
+                    alt="Promo Video"
+                    //TODO: CREATE A FUNCTION THAT CHANGES THIS URL BASED ON A NUMBER MAYBE IMPORT A VARIABLE?
+                    src="https://stream.mux.com/6fiGM5ChLz8T66ZZiuzk1KZuIKX8zJz00/medium.mp4"
+                    ref={videoEl}
+                    />
+                </div>
+        </div>
+
+    )
+}
