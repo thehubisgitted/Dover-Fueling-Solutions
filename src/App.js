304e5b61a6257f372d9b011df72d0773940c5fcd
k3v1nw4ng <kevinwang.tx@gmail.com>
2022-09-10 12:15:21 -0500

    added basic qr code to the user interface

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import '@elastic/eui/dist/eui_theme_dark.css';
+import QRCode from "react-qr-code";
 
+import { EuiFlexGroup, EuiFlexItem, EuiSpacer } from '@elastic/eui';
 function App() {
   return (
     <><EuiFlexGroup>
       <EuiFlexItem>Content grid item</EuiFlexItem>
       <EuiFlexItem>
-        <p>Another content grid item</p>
+        <p>Hello</p>
+
         <EuiSpacer />
         <p>
           Note how both of these are the same width and height despite having
           different content?
         </p>
+        <EuiSpacer />
+        <QRCode value="hey" />
       </EuiFlexItem>
     </EuiFlexGroup></>
   );
