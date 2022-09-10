f4c17175d06a990cd0c91b7d865484389ebbe7e5
yuri-0415 <eileen0415@naver.com>
2022-09-10 12:51:35 -0500

    Update Index.jsx

--- src/components/Index/Index.jsx
+++ src/components/Index/Index.jsx
@@ -3,16 +3,17 @@ import '@elastic/eui/dist/eui_theme_dark.css';
 
 import { EuiFlexGroup, EuiFlexItem, EuiSpacer, EuiPanel, EuiText } from '@elastic/eui';
 import { useState } from 'react';
+import App from '../../VideoPlayer'
 
  
 function Index(props) {
-  // const {name, setName} = useState('');
+
   return (
     <div>
     <EuiFlexGroup className="index">
       <EuiFlexItem>
         <EuiPanel paddingSize="xl">
-            Hi
+            <App />
         </EuiPanel>
       </EuiFlexItem>
 
@@ -23,7 +24,7 @@ function Index(props) {
                     Hello, {props.name}
                 </h1>
                 <p>
-                    How about having <strong>{props.product}</strong> this time? <br/>
+                    How about having <strong>{props.product}</strong> this time? <br/><br/>
                     Hold your device over a QR Code!
                 </p>
             </EuiText>
