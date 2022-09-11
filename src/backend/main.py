18e2fd18dce0f6a93ec5de532c0989a7cec6ef7a
Kenneth Pinzon <kennethpinzonrojas@gmail.com>
2022-09-10 20:55:26 -0500

    backend basics

---
+++ src/backend/main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from dotenv import dotenv_values
+from pymongo import MongoClient
+from routes import router as book_router
+
+app = FastAPI()
+
+config = dotenv_values(".env")
+
+app = FastAPI()
+
+@app.on_event("startup")
+def startup_db_client():
+    app.mongodb_client = MongoClient(config["ATLAS_URI"])
+    app.database = app.mongodb_client[config["DB_NAME"]]
+    print("Connected to the MongoDB database!")
+    
+@app.on_event("shutdown")
+def shutdown_db_client():
+    app.mongodb_client.close()
+    
+    
+app.include_router(book_router, tags =["books"], prefix="/book")
